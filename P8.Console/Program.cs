2aae265bcf5bf5c25ed2a1c2727679e1641abb8e
Mike <mlunn01@gmail.com>
2018-08-12 10:26:48 -0400

    Removed white space

--- P8.Console/Program.cs
+++ P8.Console/Program.cs
@@ -11,12 +11,10 @@ namespace P8.Console
 {
 	class Program
 	{
-
 		static void Main(string[] args)
 		{
 			System.Console.SetWindowSize(185, 45);
 			System.Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
-
 			while (true)
 			{
 				try
@@ -25,18 +23,16 @@ namespace P8.Console
 					var isNumber = int.TryParse(Read(), out int memberId);
 					var canceled = isNumber && memberId == 0;
 					if (!isNumber) throw new MemberIdException("Validation Error: Member ID must be a number.");
-
 					if (!canceled)
 					{
 						var results = Disposable.Using(() => new DiagnosisQuery(), query => query.Handle(memberId)).ToList();
-						
+
 						if (results.Any())
 						{
 							var count = results.Count();
 							var message = count > 1 ? $"Found {count} Diagnoses for Member ID {memberId}" : $"Found 1 Diagnosis for Member ID {memberId}";
 							Write(message);
 							ConsoleTable.From(results).Write(Format.Alternative);
-
 						}
 						else
 						{
@@ -47,10 +43,8 @@ namespace P8.Console
 					{
 						break;
 					}
-
-					
 				}
-				catch(MemberIdException ex)
+				catch (MemberIdException ex)
 				{
 					Thread.Sleep(TimeSpan.FromMilliseconds(100));
 					Write(ex.Message);
@@ -60,18 +54,13 @@ namespace P8.Console
 					Thread.Sleep(TimeSpan.FromMilliseconds(100));
 					Write($"The following exception was thrown during search: {ex.Message}");
 				}
-
 			}
-
 		}
-
 		static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
 		{
-			
 			e.Cancel = true;
 			System.Console.WriteLine("Cancel key press....shutting down");
 			Environment.Exit(0);
-		
 		}
 	}
 }
